03ceefbb6ab1a90eb4b7708cbe1b2e83b4c4eb16
Aleksei Borzetsov <borzetsov@gmail.com>
2026-09-12 15:45:44 +0700

    Initial commit

---
+++ src/test/java/by/tms/twitterapiproject/TwitterApiProjectApplicationTests.java
@@ -0,0 +1,15 @@
+package by.tms.twitterapiproject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
+
+@SpringBootTest
+@ActiveProfiles("test")
+class TwitterApiProjectApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
